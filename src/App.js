40f545f965fcb22078a566ed42db023067367619
Prince <prince@Princes-MacBook-Air.local>
2022-07-30 02:33:58 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Box, ChakraProvider, extendTheme, SimpleGrid } from '@chakra-ui/react';
+import Header from './components/Header';
 
 function App() {
+
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        100: "#E5FCF1",
+        200: "#27EF96",
+        300: "#10DE82",
+        400: "#0EBE6F",
+        500: "#0CA25F",
+        600: "#0A864F",
+        700: "#086F42",
+        800: "#075C37",
+        900: "#064C2E"
+      }
+    },
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Header></Header>
+
+      <SimpleGrid columns={[2, 2, 3, 4]} spacing='40px' padding='20px'>
+        <Box shadow={'2xl'} bg='tomato' height='80px'></Box>
+        <Box bg='tomato' height='80px'></Box>
+        <Box bg='tomato' height='80px'></Box>
+        <Box bg='tomato' height='80px'></Box>
+        <Box bg='tomato' height='80px'></Box>
+      </SimpleGrid>
+    </ChakraProvider>
   );
 }
 
